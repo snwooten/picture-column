d0faa82241ab648655b43dd442ff3dd549122f54
Sarah Wooten <29838661+snwooten@users.noreply.github.com>
2018-05-30 11:29:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const puppies = [
+	'http://cdn2-www.dogtime.com/assets/uploads/2010/12/puppies.jpg',
+	'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/All-about-puppies--Cesar%E2%80%99s-tips%2C-tricks-and-advice.jpg?itok=bi9xUvwe',
+	'http://cdn1-www.dogtime.com/assets/uploads/2015/05/file_21702_impossibly-cute-puppy-30-460x306.jpg',
+];
